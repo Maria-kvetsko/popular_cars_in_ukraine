8c0cc739c55e1a53f83b75463c200fbba3988668
Maria-kvetsko <m.e.r.r.y.k.v.z@gmail.com>
2025-06-10 15:56:47 +0300

    Add files via upload

---
+++ create_db.sql
@@ -0,0 +1,26 @@
+CREATE TABLE data_cars_temp (
+  person TEXT,
+  reg_addr_koatuu TEXT,
+  oper_code TEXT,
+  oper_name TEXT,
+  date_registration TEXT,
+  dep_code TEXT,
+  dep TEXT,
+  brand TEXT,
+  model TEXT,
+  vin_code TEXT,
+  make_year INTEGER,
+  color TEXT,
+  kind TEXT,
+  body TEXT,
+  purpose TEXT,
+  fuel TEXT,
+  capacity TEXT,
+  own_weight TEXT,
+  total_weight TEXT,
+  registration_number TEXT
+);
+CREATE TABLE tsc_adres(
+tsc text,
+adres_tsc text
+)

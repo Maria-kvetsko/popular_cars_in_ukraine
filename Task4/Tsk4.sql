01289b22907c148aad5c3bfb6aaa616332644d8d
Maria-kvetsko <m.e.r.r.y.k.v.z@gmail.com>
2025-06-18 17:15:10 +0300

    Task4 done

---
+++ Task4/Tsk4.sql
@@ -0,0 +1,14 @@
+--Створити процедуру яка може трансформувати машину за вказаним номером у червону фабію!
+create or replace procedure transform_to_red_fabia_in_number(numbe text)
+language SQL
+as $$
+
+	update data_cars_temp set color = 'ЧЕРВОНИЙ', brand = 'SKODA', model = 'FABIA'
+	where numbe = registration_number
+		
+$$
+
+CALL transform_to_red_fabia_in_number('КА5010НС')
+
+select * from data_cars_temp
+where registration_number = 'КА5010НС'
